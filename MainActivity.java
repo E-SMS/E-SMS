eb88cd681080f45eb30dc50ba3d3d7d09c94f507
César Gabriel Morín Corona <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-03-24 21:10:45 -0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,31 @@
-<receiver android:name=".SmsDeliveredReceiver">
-    <intent-filter>
-        <action android:name="SMS_DELIVERED" />
-    </intent-filter>
-</receiver>
+package com.example.smsservice;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText phoneNumberEditText;
+    private Button callButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phoneNumberEditText = findViewById(R.id.phoneNumber);
+        callButton = findViewById(R.id.callButton);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phoneNumberEditText.getText().toString();
+                CallService callService = new CallService(MainActivity.this);
+                callService.makeCall(phoneNumber);
+            }
+        });
+    }
+}
